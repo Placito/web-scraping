4bf8412bc4c6bc5f7b78ca325f0f085194615aad
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-29 19:53:00 +0000

    function getMovies()

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
-const cherio = require('cheerio');
+const cheerio = require('cheerio');
 
 const url = 'https://pt.wikipedia.org/wiki/Oscar_de_melhor_filme';
 
 // asynchronous function for HTTP requsio
 async function getMovies() {
-    const awayt = axios.get(url);
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
     
+    $('.wikitable tbody tr').each((i,elem) => {
+        const name = $(elem).find('td[style*="background:#FAEB86"]').last().text();
+        console.log(name);
+    });
 }
 
 getMovies();
