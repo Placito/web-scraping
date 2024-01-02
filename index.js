09e703dcaa0305c2ac512d3aebc499c53290d80b
Mariana <101410421+Placito@users.noreply.github.com>
2024-01-02 16:49:27 +0000

    join in a list

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ async function getMovies() {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
     
+    const list = [];
     $('.wikitable tbody tr').each((i,elem) => {
         const name = $(elem).find('td[style*="background:#FAEB86"]').last().text().replace('\n', '');
         if(name !== "") {
             // slice method grab the text with the value that you want
             const year = $(elem).find('td[style*="background:#FAEB86"]').first().prev('td').text().slice(-4).replace('\n', '');
             movie = { name, year }
-            console.log(movie);
-        };
+            list.push(movie);
+        }
     });
+    console.log(list);
 }
 
 getMovies();
